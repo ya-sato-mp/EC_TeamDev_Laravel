b873357f4ede42460944152cd5caba3c6f368428
k-iizuka-mediaplex <k-iizuka@mediaplex.co.jp>
2026-06-23 16:48:31 +0900

    管理者API完成

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ProductController as ApiProductController;
+use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\CartItemController;
 use App\Http\Controllers\Api\OrderController;
 use App\Http\Controllers\Admin\ProductController as AdminProductController;
 
-Route::apiResource('products', ApiProductController::class);
+Route::apiResource('products', ProductController::class);
 Route::apiResource('cart-items', CartItemController::class);
 Route::apiResource('orders', OrderController::class);
 
